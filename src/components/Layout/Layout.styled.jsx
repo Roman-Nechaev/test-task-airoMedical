18ba12006b79ddca8a6a58b5a88365f024d20314
Roman-Nechaev <nrchaevroman@gmail.com>
2023-07-22 00:32:31 +0300

    FIXED:  deploy

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -1,6 +1,15 @@
 import styled from 'styled-components';
 
-export const Background = styled.div``;
+export const Background = styled.div`
+  min-width: 100%;
+  min-height: 100vh;
+  background-image: linear-gradient(
+    to right,
+    rgba(52.5, 52.5, 73.5, 1) calc((50vw - 170px) - 340px),
+    rgba(52.5, 52.5, 73.5, 0.84) 50%,
+    rgba(52.5, 52.5, 73.5, 0.84) 100%
+  );
+`;
 
 export const Container = styled.div`
   padding-top: 60px;
